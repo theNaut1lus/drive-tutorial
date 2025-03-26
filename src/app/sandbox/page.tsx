37d7ed88e9f39b94591f18960a1665bca1793af3
Sidney Aulakh <sidneyaulakh@gmail.com>
2025-03-26 14:24:30 +1100

    db model working

---
+++ src/app/sandbox/page.tsx
@@ -0,0 +1,39 @@
+import { db } from "~/server/db";
+import { mockFiles, mockFolders } from "~/lib/mock-data";
+import { files, folders } from "~/server/db/schema";
+
+export default function Sandbox() {
+  return (
+    <div className="flex flex-col gap-4">
+      Seed Function
+      <form
+        action={async () => {
+          //this says to the client that they can use this fiunction.
+          "use server";
+          console.log("sup nerds");
+
+          const folderinsert = await db.insert(folders).values(
+            mockFolders.map((folder, index) => ({
+              id: index + 1,
+              name: folder.name,
+              parent: index !== 0 ? 1 : null,
+            })),
+          );
+          const fileinsert = await db.insert(files).values(
+            mockFiles.map((file, index) => ({
+              id: index + 1,
+              name: file.name,
+              size: 50000,
+              url: file.url,
+              parent: (index % 3) + 1,
+            })),
+          );
+          console.log("Inserted folders:", folderinsert);
+          console.log("Inserted files:", fileinsert);
+        }}
+      >
+        <button type="submit">Seed</button>
+      </form>
+    </div>
+  );
+}
--- src/server/db/schema.ts
+++ src/server/db/schema.ts
@@ -3,10 +3,45 @@ import {
   int,
   text,
   singlestoreTable,
+  index,
+  singlestoreTableCreator,
 } from "drizzle-orm/singlestore-core";
+import { url } from "inspector";
+import { SignalZero } from "lucide-react";
 
-export const users = singlestoreTable("users_table", {
-  id: bigint("id", { mode: "bigint" }).primaryKey().autoincrement(),
-  name: text("name"),
-  age: int("age"),
-});
+//to add the prefix for every table created on singlestore DB.
+export const createTable = singlestoreTableCreator(
+  (name) => `drive-tutorial_${name}`,
+);
+
+export const files = createTable(
+  "files_table",
+  {
+    //singlestore only wants bigint as it is built for scale in mind, initial push works, but subsequent fail. hence bigint.
+
+    id: bigint("id", { mode: "number", unsigned: true })
+      .primaryKey()
+      .autoincrement(),
+    name: text("name").notNull(),
+    size: int("size").notNull(),
+    url: text("url").notNull(),
+    parent: bigint("parent", { mode: "number", unsigned: true }).notNull(),
+  },
+  (tempTable) => {
+    return [index("parent_index").on(tempTable.parent)];
+  },
+);
+
+export const folders = createTable(
+  "folders_table",
+  {
+    id: bigint("id", { mode: "number", unsigned: true })
+      .primaryKey()
+      .autoincrement(),
+    name: text("name").notNull(),
+    parent: bigint("parent", { mode: "number", unsigned: true }),
+  },
+  (t) => {
+    return [index("parent_index").on(t.parent)];
+  },
+);
